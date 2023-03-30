a922d8c5cf2f8cd6c6b9eb9aa9eebb6db3c72a86
Piyush Phuyal <piyushphuyal77@gmail.com>
2023-03-30 09:41:01 +0545

    Second push

--- calc.js
+++ calc.js
@@ -1,19 +1,24 @@
-let string = "";
-let buttons = document.querySelectorAll('.btn');
-Array.from(buttons).forEach((btn)=>{
-  btn.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
-      }
-  })
-})
+function display(val){
+
+  document.getElementById('display').value += val
+
+  return val
+
+}
+
+function solve(){
+
+  let x = document.getElementById('display').value
+
+  let y = eval(x);
+
+  document.getElementById('display').value = y
+
+  return y
+
+}
+
+function clearScreen(){
+
+  document.getElementById('display').value = ''
+}
